e3c0dc19799f4b060d8399c9e8c57d42253b1047
Aditya Danturthi <aditya2096@gmail.com>
2022-03-03 23:46:57 -0330

    Loops

---
+++ Day5.py
@@ -0,0 +1,20 @@
+import math
+import os
+import random
+import re
+import sys
+    
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+
+#for i in range(1,11):
+ #   res= i*n
+  #  print(f"{n} x {i} = {res}")
+   # i += i
+i=1
+while i<11:
+    res= i*n
+    print(f"{n} x {i} = {res}")
+    i += 1
